5180ed4414a449c54e1d274a5cdba150986b890e
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-05-14 18:46:18 +0300

    Initial partially working version of offset comparison

--- src/compana.py
+++ src/compana.py
@@ -15,13 +15,13 @@ parser.add_argument('-c', '--class-code', help='specify gffcompare class code to
 arguments = parser.parse_args()
 
 gtf_paths = {
-    'gffcompare-gtf': arguments.input,
-    'reference-gtf': arguments.reference
+    'gffcompare': arguments.input,
+    'reference': arguments.reference
 }
 
 db_paths = {
-    'gffcompare-db': arguments.input[:-4] + '-ca.db',
-    'reference-db': arguments.reference[:-4] + '-ca.db'
+    'gffcompare': arguments.input[:-4] + '-ca.db',
+    'reference': arguments.reference[:-4] + '-ca.db'
 }
 
 print("=========================================")
@@ -38,7 +38,7 @@ for key, value in db_paths.items():
     else:
         print(f'{key}: creating database... this might take a while.')
         gffutils.create_db(
-            arguments.input, 
+            gtf_paths[key], 
             dbfn=f'{value}', 
             force=True, 
             keep_order=True, 
@@ -49,8 +49,8 @@ for key, value in db_paths.items():
             )
         print(f"{key}: database created successfully!")
 
-gffcompare_db = gffutils.FeatureDB(f'{db_paths["gffcompare-db"]}')
-reference_db = gffutils.FeatureDB(f'{db_paths["reference-db"]}')
+gffcompare_db = gffutils.FeatureDB(f'{db_paths["gffcompare"]}')
+reference_db = gffutils.FeatureDB(f'{db_paths["reference"]}')
 
 print("\n=========================================\n")
 
@@ -58,9 +58,9 @@ if arguments.stats:
     print("==========CLASS CODE STATISTICS==========")
     class_codes = {}
     print('\nComputing statistics for class codes...\n')
-    for feature in gffcompare_db.features_of_type('transcript'):
-        if 'class_code' in feature.attributes:
-            class_code = feature.attributes['class_code'][0]
+    for transcript in gffcompare_db.features_of_type('transcript'):
+        if 'class_code' in transcript.attributes:
+            class_code = transcript.attributes['class_code'][0]
             if not class_code in class_codes:
                 class_codes[class_code] = 0
             class_codes[class_code] += 1
@@ -78,14 +78,17 @@ if arguments.class_code:
     class_code = input("Enter class code to analyze: ")
     print(f"Analyzing class code: {class_code}")
     offset_analysis = {}
-    for feature in gffcompare_db.features_of_type('transcript'):
-        if 'class_code' in feature.attributes and class_code in feature.attributes['class_code']:
-            for exon in gffcompare_db.children(feature, featuretype='exon', order_by='start'):
-                if feature.id not in offset_analysis:
-                    offset_analysis[feature.id] = []
-                offset_analysis[feature.id].append((exon.start, exon.end))
+    for transcript in gffcompare_db.features_of_type('transcript'):
+        if 'class_code' in transcript.attributes and class_code in transcript.attributes['class_code']:
+            ref_gene_id = transcript['ref_gene_id'][0]
+            for exon in gffcompare_db.children(transcript, featuretype='exon', order_by='start'):
+                if transcript.id not in offset_analysis:
+                    offset_analysis[transcript.id] = []
+                for reference_exon in reference_db.children(ref_gene_id, featuretype='exon', order_by='start'):
+                    if reference_exon['exon_number'] == exon['exon_number'] and transcript['ref_gene_id'][0] == reference_exon['gene_id'][0]:
+                        offset_analysis[transcript.id].append((exon['exon_number'], exon.start - reference_exon.start, exon.end - reference_exon.end))
     for key, value in offset_analysis.items():
-        print(key, value)
+        print(key, sorted(value))
                 
     print("=========================================")
 
