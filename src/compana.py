3181b4d0f53fda99909c418cd1554349867395c2
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-06 17:16:07 +0300

    Add reads_bam to arguments

--- src/compana.py
+++ src/compana.py
@@ -10,27 +10,24 @@ parser = argparse.ArgumentParser(
     description='compAna: a tool for comparing annotations',
     usage='python3 compAna.py -i <input.gtf> [-f] [-s]'
     )
-parser.add_argument('-g', '--gffcompare_gtf', help='gtf-file to be imported into the database', required=False, metavar='')
-parser.add_argument('-r', '--reference_gtf', help='reference gtf-file to be compared against', required=False, metavar='')
-parser.add_argument('-a', '--reference_fasta', help='reference fasta-file to be compared against', required=False, metavar='')
+parser.add_argument('-g', '--gffcompare_gtf', help='GTF-file to be imported into the database', required=False, metavar='')
+parser.add_argument('-r', '--reference_gtf', help='reference GTF-file to be compared against', required=False, metavar='')
+parser.add_argument('-a', '--reference_fasta', help='reference FASTA-file to be compared against', required=False, metavar='')
 parser.add_argument('-o', '--offset', help='offset from which canonical splice point is to be searched', required=False, metavar='')
 parser.add_argument('-f', '--force', help='force overwrite of existing database', action='store_true')
 parser.add_argument('-s', '--stats', help='output statistics of class codes', action='store_true')
 parser.add_argument('-c', '--class-code', nargs='+', help='specify gffcompare class code to analyze.')
 parser.add_argument('-j', '--json', help='input arguments from json file', metavar='')
+parser.add_argument('-b', '--reads_bam', help='BAM-file from which reads are to be extracted from', metavar='')
 
 arguments = parser.parse_args()
 argparse_dict = vars(arguments)
-
 
 if arguments.json:
     with open(arguments.json) as json_file:
         json_dict = json.load(json_file)
         argparse_dict.update(json_dict)
         print(argparse_dict)
-
-
-    
 
 
 gtf_paths = {
