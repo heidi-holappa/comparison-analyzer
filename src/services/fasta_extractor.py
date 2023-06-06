fb450527090c2032b3f36dbcafa7976ea8377596
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-06 17:11:24 +0300

    Comment unneeded parts

--- src/services/fasta_extractor.py
+++ src/services/fasta_extractor.py
@@ -12,8 +12,8 @@ class FastaExtractor:
     
 
 
-genes = Fasta('/home/holaphei/koulutyot/lv-2022-2023/BI-summer-trainee/data/sprint-1-get-familiar-with-isoquant/reference/mouse/GRCm39.primary_assembly.genome.fa')
-print(genes.keys())
-s = genes['chr6'][87866109-4:87866109+4]
-print(s)
+# genes = Fasta('/home/holaphei/koulutyot/lv-2022-2023/BI-summer-trainee/data/sprint-1-get-familiar-with-isoquant/reference/mouse/GRCm39.primary_assembly.genome.fa')
+# print(genes.keys())
+# s = genes['chr6'][87866109-4:87866109+4]
+# print(s)
 # ENSMUST00000068755.14 
