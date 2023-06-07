0a56bb22fddb8256708e11a619748904d0e77dc6
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-07 14:47:03 +0300

    add first draft of iterator-feature

--- src/services/bam_manager.py
+++ src/services/bam_manager.py
@@ -1,16 +1,22 @@
 import os
+from pathlib import Path
+
 from services.filter_bam_multifile import create_output_filename_dict
 from services.filter_bam_multifile import create_read_dict
 from services.filter_bam_multifile import filter_reads
 from services.output_manager import default_output_manager as output_manager
+from services.cigar_parser import CigarParser
 
 
 class BamManager:
 
-    def __init__(self, bam_path: str, tsv_path: str, transcript_set: set):
+    def __init__(self, bam_path: str, tsv_path: str, matching_cases_dict: dict):
         self.bam_path = bam_path
         self.tsv_path = tsv_path
-        self.transcript_set = transcript_set
+        self.matching_cases_dict = matching_cases_dict
+        self.transcript_set = set()
+        for row in matching_cases_dict:
+            self.transcript_set.add(row[0])
         self.temporary_path = self.create_temporary_path()
 
     def execute(self):
@@ -32,6 +38,10 @@ class BamManager:
             read_dict
         )
         self.iterate_extracted_files()
+        # TODO: open each file and extract the reads
+        # TODO: compare coordinates of reads with coordinates of matching_cases_dict
+        # TODO: if there is an indel at the given position, do something. Perhaps calculate percentage of reads with indel?
+        # TODO: delete temporary files
 
     def create_temporary_path(self):
         temporary_dir = "temporary_files"
@@ -42,6 +52,11 @@ class BamManager:
         return temporary_path
 
     def iterate_extracted_files(self):
-        for file in os.listdir(self.temporary_path):
-            if file.endswith(".bam"):
-                print(file)
+        cigar_parser = CigarParser()
+        cigar_results = {}
+        for key, value in self.matching_cases_dict.items():
+            filename = Path(self.bam_path).stem + key[0] + ".bam"
+            if filename in os.listdir(self.temporary_path):
+                samfile = cigar_parser.initialize_file(filename)
+                cigar_results[filename] = cigar_parser.extract_cigar_symbol(
+                    samfile, value)
