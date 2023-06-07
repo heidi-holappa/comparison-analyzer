542af88f7878bc8c54e52fba61cc4afc529ce65f
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-07 12:45:58 +0300

    add argument for additional linebreaks

--- src/services/output_manager.py
+++ src/services/output_manager.py
@@ -1,28 +1,22 @@
 class OutputManager:
 
-    def output_line(self, line: str, is_title: bool = False, end_line="\n", fill='='):
+    def output_line(self, line: str, is_title: bool = False, end_line="\n", fill='=', additional_line_breaks=0):
         if is_title:
             line = self.generate_title(line, fill)
         print(line, end=end_line)
+        if additional_line_breaks:
+            print("\n" * additional_line_breaks)
 
     def generate_title(self, title: str, fill: str):
         line_length = 50  # This arbitrary value can be changed. Move to ENV?
-
-        # Calculate the padding length for centering
-        title_padding = (line_length - len(title)) // 2
-
-        # Pad the title line with spaces to make it centered
-        title_line = title.center(line_length, fill)
-
-        # Print the title and content
+        title_line = (" " + title + " ").center(line_length, fill)
         return title_line
 
     def output_heading(self):
         self.output_line("", is_title=True)
         self.output_line(
             "compAna: a tool for comparing annotations", fill=' ', is_title=True)
-        self.output_line("", is_title=True)
-        self.output_line("\n")
+        self.output_line("", is_title=True, additional_line_breaks=2)
 
 
 default_output_manager = OutputManager()
