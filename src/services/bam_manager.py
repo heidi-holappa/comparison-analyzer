592af7e5f9febae7afe7bc0545ebee2b44e9f691
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-07 08:44:45 +0300

    refactor. format with autopep8

--- src/services/bam_manager.py
+++ src/services/bam_manager.py
@@ -1,4 +1,6 @@
+import os
 from services.filter_bam_multifile import create_output_filename_dict, create_read_dict, filter_reads
+
 
 class BamManager:
 
@@ -8,6 +10,25 @@ class BamManager:
         self.transcript_set = transcript_set
 
     def execute(self):
-        filename_dict = create_output_filename_dict(self.bam_path, self.transcript_set)
-        read_dict = create_read_dict(self.bam_path, self.tsv_path)
-        filter_reads(self.bam_path, filename_dict, read_dict)
+        output_filename_dict = create_output_filename_dict(
+            self.bam_path,
+            self.transcript_set,
+            self.create_temporary_path()
+        )
+        read_dict = create_read_dict(
+            output_filename_dict,
+            self.tsv_path
+        )
+        filter_reads(
+            self.bam_path,
+            output_filename_dict,
+            read_dict
+        )
+
+    def create_temporary_path(self):
+        temporary_dir = "temporary_files"
+        current_dir = os.getcwd()
+        temporary_path = os.path.join(current_dir, temporary_dir)
+        if not os.path.exists(temporary_path):
+            os.makedirs(temporary_path)
+        return temporary_path
