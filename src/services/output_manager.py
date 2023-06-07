45c57a459988cea923aebf4c9f40740e67b5aba7
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-07 12:38:32 +0300

    add option to define fill-chracter

--- src/services/output_manager.py
+++ src/services/output_manager.py
@@ -1,25 +1,25 @@
 class OutputManager:
 
-    def output_line(self, line: str, is_title: bool = False, end_line="\n"):
+    def output_line(self, line: str, is_title: bool = False, end_line="\n", fill='='):
         if is_title:
-            line = self.generate_title(line)
+            line = self.generate_title(line, fill)
         print(line, end=end_line)
 
-    def generate_title(self, title: str):
+    def generate_title(self, title: str, fill: str):
         line_length = 50  # This arbitrary value can be changed. Move to ENV?
 
         # Calculate the padding length for centering
         title_padding = (line_length - len(title)) // 2
 
         # Pad the title line with spaces to make it centered
-        title_line = title.center(line_length, '=')
+        title_line = title.center(line_length, fill)
 
         # Print the title and content
         return title_line
 
     def output_heading(self):
         self.output_line("", is_title=True)
-        self.output_line("compAna: a tool for comparing annotations")
+        self.output_line("compAna: a tool for comparing annotations", fill=' ')
         self.output_line("", is_title=True)
         self.output_line("\n")
 
