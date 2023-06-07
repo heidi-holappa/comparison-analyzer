d4d7d0a838b3f00b50f794d4ee5279de33131307
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-07 12:30:07 +0300

    Add heading-generator

--- src/services/output_manager.py
+++ src/services/output_manager.py
@@ -6,7 +6,7 @@ class OutputManager:
         print(line, end=end_line)
 
     def generate_title(self, title: str):
-        line_length = 40  # This arbitrary value can be changed. Move to ENV?
+        line_length = 50  # This arbitrary value can be changed. Move to ENV?
 
         # Calculate the padding length for centering
         title_padding = (line_length - len(title)) // 2
@@ -16,6 +16,12 @@ class OutputManager:
 
         # Print the title and content
         return title_line
+
+    def output_heading(self):
+        self.output_line("", is_title=True)
+        self.output_line("compAna: a tool for comparing annotations")
+        self.output_line("", is_title=True)
+        self.output_line("\n")
 
 
 default_output_manager = OutputManager()
