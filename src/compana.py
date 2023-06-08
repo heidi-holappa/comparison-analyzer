617d8666c7344f3a7e862517869efbdb3dc2e7ce
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-08 09:48:34 +0300

    refactor to have more proper structure

--- src/compana.py
+++ src/compana.py
@@ -7,31 +7,41 @@ from services.class_code_stats import compute_class_code_stats
 from services.fasta_extractor import execute_fasta_extraction
 from services.output_manager import default_output_manager as output_manager
 
-parser = init_argparser()
 
-output_manager.output_heading()
+def run_pipeline(parser):
+    output_manager.output_heading()
 
-gffcompare_db, reference_db = init_databases(parser)
+    gffcompare_db, reference_db = init_databases(parser)
+
+    if parser.stats:
+        compute_class_code_stats(gffcompare_db)
+
+    offset_results = {}
+    if parser.class_code:
+        offset_results = execute_offset_computation(
+            parser, gffcompare_db, reference_db)
+
+    matching_cases_dict = {}
+
+    if parser.reference_fasta:
+        matching_cases_dict = execute_fasta_extraction(
+            parser, offset_results, reference_db)
+
+    if parser.reads_tsv and parser.reads_bam:
+        transcripts = set()
+        bam_manager = BamManager(
+            parser.reads_bam,
+            parser.reads_tsv,
+            matching_cases_dict
+        )
+        bam_manager.execute()
+    output_manager.output_footer()
 
 
-if parser.stats:
-    compute_class_code_stats(gffcompare_db)
+def main():
+    parser = init_argparser()
+    run_pipeline(parser)
 
-offset_results = {}
-if parser.class_code:
-    offset_results = execute_offset_computation(
-        parser, gffcompare_db, reference_db)
 
-matching_cases_dict = {}
-
-if parser.reference_fasta:
-    matching_cases_dict = execute_fasta_extraction(
-        parser, offset_results, reference_db)
-
-if parser.reads_tsv and parser.reads_bam:
-    transcripts = set()
-    bam_manager = BamManager(
-        parser.reads_bam, parser.reads_tsv, matching_cases_dict)
-    bam_manager.execute()
-
-print("================= END ===================")
+if __name__ == "__main__":
+    main()
