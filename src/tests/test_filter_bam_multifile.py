43d2deed9fa7d1be1aa51acdb7f16e30f7f2e1bc
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-09 09:38:36 +0300

    add test for filtering reads

--- src/tests/test_filter_bam_multifile.py
+++ src/tests/test_filter_bam_multifile.py
@@ -4,6 +4,9 @@ from unittest import TestCase
 from services.filter_bam_multifile import create_read_dict
 from services.filter_bam_multifile import create_output_filename_dict_cli
 from services.filter_bam_multifile import create_output_filename_dict
+from services.filter_bam_multifile import filter_reads
+from tests.sample_file_management import default_test_file_manager as file_manager
+
 
 from config import TEMPORARY_DIR
 
@@ -54,7 +57,6 @@ class TestCreateFilenameDictionary(TestCase):
         transcript_set = {"transcript1", "transcript2"}
         output_filename_dict = create_output_filename_dict(
             bam_file, transcript_set, TEMPORARY_DIR)
-        print(output_filename_dict)
         self.assertEqual(len(output_filename_dict), 2)
 
     def test_cli_version_output_filename_dict_has_correct_number_of_keys(self):
@@ -64,8 +66,31 @@ class TestCreateFilenameDictionary(TestCase):
             self.transcript_list_file,
             suffix="test"
         )
-        print(output_filename_dict)
         self.assertEqual(len(output_filename_dict), 3)
+
+    def tearDown(self) -> None:
+        if os.path.exists(TEMPORARY_DIR):
+            shutil.rmtree(TEMPORARY_DIR)
+
+
+class TestFilterBamFile(TestCase):
+
+    def setUp(self):
+        if not os.path.exists(TEMPORARY_DIR):
+            os.makedirs(TEMPORARY_DIR)
+        self.file_manager = file_manager
+        self.bam_file = self.file_manager.bam_file
+
+    def test_filter_bam_file_produces_output_bam_file(self):
+        output_filename = os.path.join(TEMPORARY_DIR, "transcript1.bam")
+        output_filename_dict = {
+            "transcript925.ch1.nnic": output_filename
+        }
+        dict_of_reads = {
+            "ENSMUST00000208994_962_aligned_8455583_F_112_261_0": ["transcript925.ch1.nnic"],
+        }
+        filter_reads(self.bam_file, output_filename_dict, dict_of_reads)
+        self.assertTrue(os.path.exists(output_filename))
 
     def tearDown(self) -> None:
         if os.path.exists(TEMPORARY_DIR):
