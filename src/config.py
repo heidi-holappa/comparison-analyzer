a6710562301982a670330c0cbe4a996dbf5d7a1a
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-09 16:38:30 +0300

    add log file

--- src/config.py
+++ src/config.py
@@ -15,6 +15,7 @@ test_file_directory = os.getenv(
 
 log_dir = os.getenv("LOG_FILE_DIR") or "logs"
 fasta_overview = os.getenv("FASTA_OVERVIEW_FILE") or "fasta_overview.md"
+cigar_results = os.getenv("CIGAR_RESULTS_LOG") or "cigar_results.md"
 LOG_FILE_DIR = os.path.join(dirname, "..", log_dir)
 if not os.path.exists(LOG_FILE_DIR):
     os.mkdir(LOG_FILE_DIR)
@@ -23,4 +24,5 @@ TEMPORARY_DIR = os.path.join(dirname, "..", temporary_dir_path)
 OFFSET_LOG = os.path.join(LOG_FILE_DIR, offset_log)
 TEST_FILE_DIR = os.path.join(dirname, "..", test_file_directory)
 FASTA_OVERVIEW_FILE = os.path.join(LOG_FILE_DIR, fasta_overview)
+CIGAR_RESULTS_LOG = os.path.join(LOG_FILE_DIR, cigar_results)
 TITLE_FILE_LENGTH = os.getenv("TITLE_FILE_LENGTH") or 50
