4bdeba665b5cb09a49c245d34d3579bb77e7d8c4
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-09 10:51:27 +0300

    update tests to use a class

--- src/services/class_code_stats.py
+++ src/services/class_code_stats.py
@@ -1,17 +1,25 @@
-def compute_class_code_stats(gffcompare_db):
-    print("==========CLASS CODE STATISTICS==========")
-    class_codes = {}
-    print('\nComputing statistics for class codes...\n')
-    for transcript in gffcompare_db.features_of_type('transcript'):
-        if 'class_code' in transcript.attributes:
-            class_code = transcript.attributes['class_code'][0]
-            if not class_code in class_codes:
-                class_codes[class_code] = 0
-            class_codes[class_code] += 1
+from services.output_manager import default_output_manager as output_manager
 
-    print(f"{'class code':<18}| {'n':<10}")
-    print('-' * 30)
-    for key, value in sorted(class_codes.items(), key=lambda item: item[1], reverse=True):
-        print(f"{key:<18}| {value:<10}")
 
-    print("\n=========================================")
+class ClassCodeStats:
+
+    def __init__(self, gffcompare_db):
+        self.gffcompare_db = gffcompare_db
+
+    def compute_class_code_stats(self):
+        output_manager.output_line("CLASS CODE STATISTICS", is_title=True)
+        class_codes = {}
+        for transcript in self.gffcompare_db.features_of_type('transcript'):
+            if 'class_code' in transcript.attributes:
+                class_code = transcript.attributes['class_code'][0]
+                if not class_code in class_codes:
+                    class_codes[class_code] = 0
+                class_codes[class_code] += 1
+
+        output_manager.output_line(
+            f"{'class code':<18}| {'n':<10}")
+        output_manager.output_line("", is_title=True, fill="-")
+        for key, value in sorted(class_codes.items(), key=lambda item: item[1], reverse=True):
+            output_manager.output_line(f"{key:<18}| {value:<10}")
+
+        output_manager.output_line("", is_title=True)
