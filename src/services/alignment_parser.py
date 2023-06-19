ff0dc76aa577ce88fcd87fcf5b71297314a036b3
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-19 10:53:59 +0300

    Fix issue with misalignment

--- src/services/alignment_parser.py
+++ src/services/alignment_parser.py
@@ -52,63 +52,10 @@ class AlignmentParser:
 
         return -1
 
-    def create_updated_cigar_tuples(self, cigar_tuples: list, ):
-        cigar_code_list = []
-        index_counter = 0
-
-        for cigar_tuple in cigar_tuples:
-            for _ in range(cigar_tuple[1]):
-                cigar_code_list.append((cigar_tuple[0], index_counter))
-                index_counter += 1
-
-        return cigar_code_list
-
-    def get_cigar_codes_in_window(self, cigar_code_list: list, aligned_location: int, loc_type: str):
-        """
-        Get cigar codes in a given window.
-
-        Args:
-            cigar_tuples (list): list of cigar tuples (cigar code, aligned position)
-            aligned_location (int): aligned location
-            loc_type (str): type of location (start or end)
-        """
-        deletions = 0
-        insertions = 0
-        debug_list = []
-
-        if loc_type == "end":
-            for cigar_code in cigar_code_list[aligned_location - self.window_size:aligned_location]:
-                debug_list = cigar_code_list[aligned_location -
-                                             self.window_size:aligned_location]
-                if cigar_code[0] == 2:
-                    deletions += 1
-                if cigar_code[0] == 1:
-                    insertions += 1
-        elif loc_type == "start":
-            debug_list = cigar_code_list[aligned_location:
-                                         aligned_location + self.window_size]
-            for cigar_code in cigar_code_list[aligned_location:aligned_location + self.window_size]:
-                if cigar_code[0] == 2:
-                    deletions += 1
-                if cigar_code[0] == 1:
-                    insertions += 1
-
-        if deletions:
-            if deletions not in self.case_count["deletions"]:
-                self.case_count["deletions"][deletions] = 0
-            self.case_count["deletions"][deletions] += 1
-        if insertions:
-            if insertions not in self.case_count["insertions"]:
-                self.case_count["insertions"][insertions] = 0
-            self.case_count["insertions"][insertions] += 1
-
-        if deletions >= self.window_size or insertions >= self.window_size:
-            debug_list.append(
-                f"deletions: {deletions}, insertions: {insertions}")
-            return True, debug_list
-        return False, debug_list
-
-    def fetch_cigar_codes_from_given_window(self, cigar_tuples: list, aligned_location: int, loc_type: str):
+    def fetch_cigar_codes_from_given_window(self,
+                                            cigar_tuples: list,
+                                            aligned_location: int,
+                                            loc_type: str):
         """
         Get cigar codes in a given window.
 
@@ -124,20 +71,21 @@ class AlignmentParser:
 
         cigar_code_list = []
 
-        if loc_type == "start":
-            aligned_location = aligned_location + self.window_size
+        if loc_type == "end":
+            aligned_location = aligned_location - self.window_size + 1
 
         for cigar_code in cigar_tuples:
             if self.window_size == len(cigar_code_list):
                 break
             if location + cigar_code[1] > aligned_location:
-                overlap = location + cigar_code[1] - aligned_location
+                overlap = location + \
+                    cigar_code[1] - (aligned_location + len(cigar_code_list))
                 cigar_code_list.extend(
                     [cigar_code[0] for _ in range(min(self.window_size - len(cigar_code_list), overlap))])
-                location += cigar_code[1]
+            location += cigar_code[1]
 
+        debug_list = cigar_code_list
         for cigar_code in cigar_code_list:
-            debug_list = cigar_code_list
             if cigar_code == 2:
                 deletions += 1
             if cigar_code == 1:
@@ -200,7 +148,7 @@ class AlignmentParser:
             debug_list.append(
                 f"deletions: {deletions}, insertions: {insertions}")
             return True, debug_list
-        return False, debug_list
+        return True, debug_list
 
     def write_alignment_errors_to_file(self, errors: list):
         with open(self.error_file_output_dir, "w") as file:
@@ -284,3 +232,59 @@ class AlignmentParser:
 
 
 default_alignment_parser = AlignmentParser()
+
+# def create_updated_cigar_tuples(self, cigar_tuples: list, ):
+#     cigar_code_list = []
+#     index_counter = 0
+
+#     for cigar_tuple in cigar_tuples:
+#         for _ in range(cigar_tuple[1]):
+#             cigar_code_list.append((cigar_tuple[0], index_counter))
+#             index_counter += 1
+
+#     return cigar_code_list
+
+# def get_cigar_codes_in_window(self, cigar_code_list: list, aligned_location: int, loc_type: str):
+#     """
+#     Get cigar codes in a given window.
+
+#     Args:
+#         cigar_tuples (list): list of cigar tuples (cigar code, aligned position)
+#         aligned_location (int): aligned location
+#         loc_type (str): type of location (start or end)
+#     """
+#     deletions = 0
+#     insertions = 0
+#     debug_list = []
+
+#     if loc_type == "end":
+#         for cigar_code in cigar_code_list[aligned_location - self.window_size:aligned_location]:
+#             debug_list = cigar_code_list[aligned_location -
+#                                          self.window_size:aligned_location]
+#             if cigar_code[0] == 2:
+#                 deletions += 1
+#             if cigar_code[0] == 1:
+#                 insertions += 1
+#     elif loc_type == "start":
+#         debug_list = cigar_code_list[aligned_location:
+#                                      aligned_location + self.window_size]
+#         for cigar_code in cigar_code_list[aligned_location:aligned_location + self.window_size]:
+#             if cigar_code[0] == 2:
+#                 deletions += 1
+#             if cigar_code[0] == 1:
+#                 insertions += 1
+
+#     if deletions:
+#         if deletions not in self.case_count["deletions"]:
+#             self.case_count["deletions"][deletions] = 0
+#         self.case_count["deletions"][deletions] += 1
+#     if insertions:
+#         if insertions not in self.case_count["insertions"]:
+#             self.case_count["insertions"][insertions] = 0
+#         self.case_count["insertions"][insertions] += 1
+
+#     if deletions >= self.window_size or insertions >= self.window_size:
+#         debug_list.append(
+#             f"deletions: {deletions}, insertions: {insertions}")
+#         return True, debug_list
+#     return False, debug_list
