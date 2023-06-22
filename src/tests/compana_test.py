ae95aa34a0eaf7501e800809a18e0282032c1f47
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-22 15:47:28 +0300

    add unittest and related json

--- src/tests/compana_test.py
+++ src/tests/compana_test.py
@@ -1,3 +1,9 @@
+# Consider how to best test the main function
+# The issue is that now this test runs the whole pipeline
+# and 'covers' most of the code. However, it is not a unit test.
+# It is more of an integration test.
+# Consider mocking the functions that are called in main
+
 import sys
 from unittest import TestCase
 
@@ -10,9 +16,16 @@ class TestCompana(TestCase):
         self.execute = main
         pass
 
-    def test_compana_runs_without_errors(self):
+    def test_compana_runs_without_errors_with_all_args(self):
         sys.argv = [
             'compAna.py',
             '-j=src/tests/files/test-run.json'
+        ]
+        self.execute()
+
+    def test_compana_runs_without_errors_with_limited_args(self):
+        sys.argv = [
+            'compAna.py',
+            '-j=src/tests/files/test-run-fewer-args.json'
         ]
         self.execute()
