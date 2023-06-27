c8397699df100c3590d7196cd8f5b8ae11c66820
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-27 10:47:16 +0300

    normalize graph-values

--- src/services/graph_manager.py
+++ src/services/graph_manager.py
@@ -12,8 +12,15 @@ class GraphManagement:
         if not os.path.exists(self.graph_dir):
             os.mkdir(self.graph_dir)
 
+    def normalize_values(self, graph_values: dict):
+        factor = 1.0 / sum(graph_values.values())
+        normalized_graph_values = {
+            k: v*factor for k, v in graph_values.items()}
+        return normalized_graph_values
+
     def construct_bar_chart_from_dict(self, graph_values: dict, title: str, x_label: str, y_label: str):
-        values = sorted(graph_values.items())
+        normalized_graph_values = self.normalize_values(graph_values)
+        values = sorted(normalized_graph_values.items())
         x_values, y_values = zip(*values)
 
         plt.bar(x_values, height=y_values, tick_label=x_values)
