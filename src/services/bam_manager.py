eb0e3c3da4906cff0eb1f1567cd71d36e74061db
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-06-27 13:13:15 +0300

    Improve stdout

--- src/services/bam_manager.py
+++ src/services/bam_manager.py
@@ -107,10 +107,10 @@ class BamManager:
         #     "is_info": True
         # })
         for key, value in alignment_parser.updated_case_count.items():
-            title = str(key[0]) + ".strand-" + str(key[1]) + ".exon-loc-" + \
+            title = str(key[0]) + ".strand_" + str(key[1]) + ".exon-loc-" + \
                 str(key[2]) + ".offset-(" + str(key[3]) + ")"
             output_manager.output_line({
-                "line": f"in/del: {key[0]}, strand: {key[1]}, : {key[2]}, offset: {key[3]}: {value}",
+                "line": f"in/del: {key[0]}, strand: {key[1]}, exon location: {key[2]}, offset: {key[3]}: {value}",
                 "is_info": True
             })
             graph_manager.construct_bar_chart_from_dict(
