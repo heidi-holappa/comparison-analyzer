ba65e87d3abe970017c38aae0f8b988e9a17c528
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-07-19 10:21:42 +0300

    initial commit

---
+++ src/implementation_services/case_extractor.py
@@ -0,0 +1,73 @@
+from services.output_manager import default_output_manager as output_manager
+
+
+class CasesExtractor:
+
+    def __init__(self, offset_results: dict, offset_range: tuple, isoquant_db):
+        """
+            Extract candidates matching the selected offset. Currently first and last 
+            exon are not considered. These are omitted with the magic numbers in the 
+            second for loop.
+
+        Args:
+            offset_results (dict): a dictionary containing the offset results for each transcript
+            offset (int): the offset to match
+            reference_db: reference to a gffutils database with the reference annotation
+        """
+        self.offset_results = offset_results
+        self.offset_range = offset_range
+        self.isoquant_db = isoquant_db
+
+    def extract_candidates_matching_selected_offset(self) -> dict:
+        output_manager.output_line({
+            "line": "IMPLEMENTATION: EXTRACTING CASES",
+            "is_title": True
+        })
+        output_manager.output_line({
+            "line": f"Extracting cases from the IsoQuant gtf-db",
+            "is_info": True
+        })
+        extracted_cases = {}
+        for transcript_id, value in self.offset_results.items():
+            strand, offsets, reference_id = value["strand"], value["offsets"], value["reference_id"]
+            for offset_exon_idx in range(1, len(offsets)-1):
+                start_is_in_range = bool(
+                    abs(offsets[offset_exon_idx][0]) >= self.offset_range[0] and
+                    abs(offsets[offset_exon_idx][0]) <= self.offset_range[1])
+                if start_is_in_range:
+                    entry_key = transcript_id + ".exon_" + \
+                        str(offset_exon_idx) + ".start" + \
+                        '.offset_' + str(offsets[offset_exon_idx][0])
+                    for exon_number, exon in enumerate(self.isoquant_db.children(
+                            reference_id, featuretype='exon', order_by='start')):
+                        if exon_number == offset_exon_idx:
+                            extracted_cases[entry_key] = {
+                                "transcript_id": transcript_id,
+                                "strand": strand,
+                                "exon_number": offset_exon_idx,
+                                "location_type": "start",
+                                "location": exon.start + offsets[offset_exon_idx][0],
+                                "offset": offsets[offset_exon_idx][0]
+                            }
+                end_is_in_range = bool(abs(offsets[offset_exon_idx][1]) >= self.offset_range[0] and
+                                       abs(offsets[offset_exon_idx][1]) <= self.offset_range[1])
+                if end_is_in_range:
+                    entry_key = transcript_id + ".exon_" + \
+                        str(offset_exon_idx) + ".end" + \
+                        '.offset_' + str(offsets[offset_exon_idx][1])
+                    for exon_number, exon in enumerate(self.isoquant_db.children(
+                            reference_id, featuretype='exon', order_by='start')):
+                        if exon_number == offset_exon_idx:
+                            extracted_cases[entry_key] = {
+                                "transcript_id": transcript_id,
+                                "strand": strand,
+                                "exon_number": offset_exon_idx,
+                                "location_type": "end",
+                                "location": exon.end + offsets[offset_exon_idx][1],
+                                "offset": offsets[offset_exon_idx][1]
+                            }
+        output_manager.output_line({
+            "line": f"Found {len(extracted_cases)} candidates in the given offset range",
+            "is_info": True
+        })
+        return extracted_cases
