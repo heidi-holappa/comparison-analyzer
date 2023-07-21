ab37dbcd174a399c43e078136f8509bb658c90b8
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-07-21 13:45:20 +0300

    fix issue with wrong method-call

--- src/implementation_services/verify_results.py
+++ src/implementation_services/verify_results.py
@@ -10,7 +10,7 @@ def verify_results(intron_site_dict: dict, matching_cases_dict: dict):
         'TP': 0,
         'FP': 0
     }
-    for key, value in intron_site_dict.values():
+    for key, value in intron_site_dict.items():
         if value['extracted_information']['right']['error_detected'] or \
                 value['extracted_information']['left']['error_detected']:
             offset = matching_cases_dict[key]['offset']
