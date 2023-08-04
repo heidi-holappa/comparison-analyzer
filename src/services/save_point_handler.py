35c4691ea7c570c84d55f8e8eb652683ac6a8463
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-08-04 13:47:40 +0300

    add second save point

--- src/services/save_point_handler.py
+++ src/services/save_point_handler.py
@@ -1,8 +1,5 @@
 import os
-from pathlib import Path
 import pickle
-
-from config import LOG_DIR
 
 
 def get_matching_cases(save_file: str):
@@ -16,3 +13,15 @@ def get_matching_cases(save_file: str):
 def save_matching_cases(save_file: str, matching_cases_dict: dict):
     with open(save_file, "wb") as f:
         pickle.dump(matching_cases_dict, f)
+
+
+def get_intron_cases(intron_save_file: str):
+    if os.path.exists(intron_save_file):
+        with open(intron_save_file, "rb") as f:
+            return pickle.load(f)
+    return None
+
+
+def save_intron_cases(intron_save_file: str, intron_site_dict: dict):
+    with open(intron_save_file, "wb") as f:
+        pickle.dump(intron_site_dict, f)
