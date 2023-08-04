13a6b13706c0911e2eb3bd126ea39f79795e4b71
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-08-04 18:49:18 +0300

    fix issue with datatype

--- src/implementation_services/closest_canonicals_extractor.py
+++ src/implementation_services/closest_canonicals_extractor.py
@@ -28,8 +28,8 @@ def extract_nucleotides_from_most_common_del_location(dict_entry: dict, fasta: F
         location = dict_entry["location"]
         coordinates = (chr, location + most_common_right,
                        location + most_common_right + 2)
-        dict_entry["extracted_information"]["right"]["most_common_case_nucleotides"] = extract_characters_at_given_coordinates(
-            coordinates, -1, fasta)
+        dict_entry["extracted_information"]["right"]["most_common_case_nucleotides"] = str(extract_characters_at_given_coordinates(
+            coordinates, -1, fasta))
     if most_common_left:
         chr = dict_entry["seq_id"]
         location = dict_entry["location"]
