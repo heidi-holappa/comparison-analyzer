f26f3f26f610ff03ec1689e0a96534ac67ea1ff7
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-08-08 14:26:50 +0300

    add indel computation for pkl-files

--- saved_results/intron-case-analyzer.py
+++ saved_results/intron-case-analyzer.py
@@ -1,13 +1,15 @@
 import os
+from collections import defaultdict
+from pathlib import Path
 import pickle
 
-directory = 'saved_results'
+directory = '/home/holaphei/koulutyot/lv-2022-2023/BI-summer-trainee/isoquant-summer-trainee/ca-task-2/comparison-analyzer/saved_results'
 
 files = []
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     if os.path.isfile(f):
-        if 'intron_cases' in f:
+        if 'intron-cases' in f:
             files.append(f)
 
 
@@ -18,11 +20,32 @@ def get_intron_cases(intron_save_file: str):
     return None
 
 
+def count_most_common_indel_case(indel_dict: dict):
+    most_common_case = [
+        k for k, v in indel_dict.items() if v == max(indel_dict.values())]
+    if len(most_common_case) > 1 or len(most_common_case) == 0:
+        return -1
+    return most_common_case[0]
+
+
+results = {}
+
 for file in files:
     intron_cases = get_intron_cases(file)
     max_del_count = {
-        'left': {},
-        'right': {}
+        'left': defaultdict(int),
+        'right': defaultdict(int)
     }
+    if not isinstance(intron_cases, dict):
+        continue
     for value in intron_cases.values():
-        for 
+        for direction in ['left', 'right']:
+            dels = value['extracted_information'][direction]['deletions']
+            most_common_del = count_most_common_indel_case(dels)
+            max_del_count[direction][most_common_del] += 1
+    filename = Path(file).stem
+    results[filename] = max_del_count
+
+for filename, output in results.items():
+    for direction, cases in output.items():
+        print(filename, direction, dict(cases))
